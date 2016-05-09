67c94c0e9a335468e3e95829aacd063911dcbb56
Rachasak Ragkamnerd <itpcc@users.noreply.github.com>
2016-05-09 18:46:05 +0700

    Add files via upload

--- blognone-hide-comment.js
+++ blognone-hide-comment.js
@@ -17,14 +17,17 @@
 				jQuery(this).find(".comment-info").append(' | <a class="show-hide-comment" href="javascript:void;" style="color: white;" data-commentid="'+jQuery(this).attr("id")+'">show</a>');
 			});
 			jQuery(".show-hide-comment[data-commentid]").live("click", function(e){
+				var nextSib = jQuery("#"+jQuery(this).attr("data-commentid")).next(".indented").eq(0);
 				e.preventDefault();
 				if(jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").is(":visible")){
 					jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").hide();
-					jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").hide();
+					if(nextSib.hasClass("indented"))
+						nextSib.hide();
 					jQuery(this).html("hide");
 				}else{
 					jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").show();
-					jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").show();
+					if(nextSib.hasClass("indented"))
+						nextSib.hide();
 					jQuery(this).html("show");
 				}
 			});
