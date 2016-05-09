f5997c98f5aab677609c8ae9077ce8d6841f123d
Rachasak Ragkamnerd <itpcc@users.noreply.github.com>
2016-05-09 18:23:20 +0700

    Add files via upload

--- blognone-hide-comment.js
+++ blognone-hide-comment.js
@@ -1,17 +1,33 @@
-jQuery(document).ready(function() {
-	jQuery(".comment").each(function(){
-		jQuery(this).find(".comment-info").append(' | <a class="show-hide-comment" href="javascript:void;" style="color: white;" data-commentid="'+jQuery(this).attr("id")+'">show</a>');		
-	});
-	jQuery(".show-hide-comment[data-commentid]").live("click", function(e){
-		e.preventDefault();
-		if(jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").is(":visible")){
-			jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").hide();
-			jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").hide();
-			jQuery(this).html("hide");
-		}else{
-			jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").show();
-			jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").show();
-			jQuery(this).html("show");
+// ==UserScript==
+// @name         Blognone hide show comment
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Rachasak Ragkamnerd
+// @match        https://www.blognone.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    jQuery(document).ready(function() {
+    	if(jQuery(".comment").length){
+			jQuery(".comment").each(function(){
+				jQuery(this).find(".comment-info").append(' | <a class="show-hide-comment" href="javascript:void;" style="color: white;" data-commentid="'+jQuery(this).attr("id")+'">show</a>');
+			});
+			jQuery(".show-hide-comment[data-commentid]").live("click", function(e){
+				e.preventDefault();
+				if(jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").is(":visible")){
+					jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").hide();
+					jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").hide();
+					jQuery(this).html("hide");
+				}else{
+					jQuery("#"+jQuery(this).attr("data-commentid")+" .comment-body").show();
+					jQuery("#"+jQuery(this).attr("data-commentid")+" ~ .indented").show();
+					jQuery(this).html("show");
+				}
+			});
 		}
 	});
-});
+})();
